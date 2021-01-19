b1e20a868eed374357b8d7a1f0f6655df2b6535e
Rmiverson <Rmiverson99@gmail.com>
2021-01-19 10:35:10 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+const submitData = (userName, email) => {
+   let data = {
+      name: userName,
+      email: email
+   }
+
+   let configObj = {
+      method: "POST",
+      headers: {
+         "Content-Type": "application/json",
+         "Accept": "application/json"
+      },
+      body: JSON.stringify(data)
+   }
+
+   return fetch('http://localhost:3000/users', configObj)
+   .then((response) => {
+      return response.json()
+   })
+   .then((obj) => {
+      return addToBody('p', obj.id)
+   })
+   .catch((error) => {
+      return addToBody('p', error.message)
+   })
+}
+
+// helper that adds specified tag as a string to the body of the document with data
+const addToBody = (tag, data) => {
+   const body = document.querySelector('body')
+   const t = document.createElement(tag)
+   t.innerHTML = data
+   body.appendChild(t)
+}
